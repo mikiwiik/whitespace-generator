1ef3e49cfcd7334133383b7a9934be24c9385f95
miki <miki.wiik@gmail.com>
2012-08-27 22:45:38 +0300

    Create class for representing a WhiteSpace program with all the necessary methods to create the worlds simplest hello world

--- src/main/java/com/github/mikiwiik/PrinterTester.java
+++ src/main/java/com/github/mikiwiik/PrinterTester.java
@@ -2,18 +2,18 @@ package com.github.mikiwiik;
 
 public class PrinterTester {
     public static void main(String[] args) throws Exception {
-        String wspace =
-            "   LF:1010\t \t \n\r"+
-            "   j:1101010\t\t \t \t \n\r"+
-            "   e:1100101\t\t  \t \t\n\r"+
-            "   H:1001000\t  \t   \n\r"+
-            "\t\n  \r"+
-            "\t\n  \r"+
-            "\t\n  \r"+
-            "\t\n  \r"+
-            "\n\n\n";
+        String program = new WhiteSpaceProgram()
+            .stackPushString(" LF:1010\t \t \n")
+            .stackPushString(" j:1101010\t\t \t \t \n")
+            .stackPushString(" e:1100101\t\t  \t \t\n")
+            .stackPushString(" H:1001000\t  \t   \n")
+            .ioPopChar()
+            .ioPopChar()
+            .ioPopChar()
+            .ioPopChar()
+            .flowEndProgram();
 
-        String shellOutput = ShellHelper.runWSpaceInShell(wspace);
+        String shellOutput = ShellHelper.runWSpaceInShell(program);
         System.out.print(shellOutput);
     }
 }
