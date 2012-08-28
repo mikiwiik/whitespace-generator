10bb9b05234b3cc17a7707057bfe617ae3cb71e7
miki <miki.wiik@gmail.com>
2012-08-28 23:01:38 +0300

    Use full 8-bits (though the highest ones are not used)

--- src/main/java/com/github/mikiwiik/PrinterTester.java
+++ src/main/java/com/github/mikiwiik/PrinterTester.java
@@ -3,10 +3,10 @@ package com.github.mikiwiik;
 public class PrinterTester {
     public static void main(String[] args) throws Exception {
         String program = new WhiteSpaceProgram()
-            .stackPushString(" LF:1010\t \t \n")
-            .stackPushString(" j:1101010\t\t \t \t \n")
-            .stackPushString(" e:1100101\t\t  \t \t\n")
-            .stackPushString(" H:1001000\t  \t   \n")
+            .stackPushString(" LF:00001010    \t \t \n")
+            .stackPushString(" j:01101010 \t\t \t \t \n")
+            .stackPushString(" e:01100101 \t\t  \t \t\n")
+            .stackPushString(" H:01001000 \t  \t   \n")
             .ioPopChar()
             .ioPopChar()
             .ioPopChar()
