737f0d10ba5a8d05c974c6c4d0e60d4ca47f81f1
miki <miki.wiik@gmail.com>
2012-09-02 18:45:31 +0300

    Remove unnecessary stuff

--- src/main/java/com/github/mikiwiik/BitUtils.java
+++ src/main/java/com/github/mikiwiik/BitUtils.java
@@ -1,39 +1,9 @@
 package com.github.mikiwiik;
 
-import com.google.common.collect.ImmutableList;
-
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-
 /**
  * Miscellaneous bit-operation related util methods
  */
 public class BitUtils {
-
-    /**
-     * Convert the given String into Lists of boolean values representing the String as Ascii bits
-     * Inspired by
-     *      <a href="http://stackoverflow.com/questions/917163/convert-a-string-like-testing123-to-binary-in-java">
-     *      http://stackoverflow.com/questions/917163/convert-a-string-like-testing123-to-binary-in-java</a>
-     * @param string
-     * @return
-     * @throws UnsupportedEncodingException
-     */
-    public static List<Boolean> asciiAsBinary(String string) throws UnsupportedEncodingException {
-
-        final ImmutableList.Builder<Boolean> bits = new ImmutableList.Builder<Boolean>();
-
-        // Could do byte[] bytes = string.getBytes("US-ASCII");
-        byte[] bytes = string.getBytes();
-        for (byte b : bytes) {
-            int val = b;
-            for (int i = 0; i < 8; i++) {
-                bits.add((val & 128) != 0);
-                val <<= 1;
-            }
-        }
-        return bits.build();
-    }
 
     /**
      * Convert the given char into a String containing whitespace representation of a character number.<br/>
--- src/test/java/com/github/mikiwiik/BitUtilsTest.java
+++ src/test/java/com/github/mikiwiik/BitUtilsTest.java
@@ -1,24 +1,11 @@
 package com.github.mikiwiik;
 
-import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class BitUtilsTest {
 
-    @Test
-    public void testConvertingSingleCharacters() throws Exception {
-        // LF:000001010
-
-        assertEquals("j:01101010",
-                Lists.newArrayList(false,true,true,false,true,false,true,false),
-                BitUtils.asciiAsBinary("j"));
-
-        assertEquals("H:01001000",
-                Lists.newArrayList(false,true,false,false, true,false,false,false),
-                BitUtils.asciiAsBinary("H"));
-    }
     @Test
     public void testConvertingSingleChars() throws Exception {
         assertEquals("LF: 00001010    \t \t ",
