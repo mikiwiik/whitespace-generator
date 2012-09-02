5c4864499719269816d0f731a5acad426f28e1df
miki <miki.wiik@gmail.com>
2012-09-02 13:57:18 +0300

    Added test fof I/O, pop single char

--- src/test/java/com/github/mikiwiik/WhiteSpaceProgramTest.java
+++ src/test/java/com/github/mikiwiik/WhiteSpaceProgramTest.java
@@ -12,4 +12,12 @@ public class WhiteSpaceProgramTest {
         assertEquals("SS(Stack IMP, push nr) S(pos nr) STTSTSTS(j) CR",
             "   \t\t \t \t \r", program);
     }
+
+    @Test
+    public void testPopSingleChar() {
+        final String program = new WhiteSpaceProgram().ioPopChar().toString();
+        assertEquals("[Tab][LF](I/O IMP), [Space][Space] Output the character at the top of the stack, CR",
+                "\t\n  \r", program);
+    }
+
 }
